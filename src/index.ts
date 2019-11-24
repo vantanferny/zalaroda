a4d9a2fe00605bfad25a7bb829a28f14ffae8ef1
Anferny Vanta <vantanferny@github.com>
2019-11-24 14:35:07 +0800

    added node types plus home page w/ hello world

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("It's a boiishitiiii")
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`)
+})
