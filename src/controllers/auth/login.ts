b898f0babf7a63eab50c31bf529dabfa8ba8b825
Anferny Vanta <vantanferny@github.com>
2020-02-23 14:44:12 +0800

    created auth front end layer

--- src/controllers/auth/login.ts
+++ src/controllers/auth/login.ts
@@ -6,4 +6,16 @@ login.get('/', (req, res) => {
   res.render('auth/login')
 })
 
+login.post('/', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const userInput = {
+    email: email,
+    password: password,
+  }
+
+  res.json(userInput)
+})
+
 export default login
--- src/controllers/auth/signup.ts
+++ src/controllers/auth/signup.ts
@@ -6,4 +6,22 @@ signup.get('/', (req, res) => {
   res.render('auth/signup')
 })
 
+signup.post('/', (req, res) => {
+  const email: string = req.body.email
+  const firstName: string = req.body.first_name.trim()
+  const lastName: string = req.body.last_name.trim()
+  const name: string = firstName + " " + lastName
+  const mobileNumber: string = req.body.mobile_number
+  const password: string = req.body.password
+
+  const userInput = {
+    email: email,
+    password: password,
+    name: name,
+    mobileNumber: mobileNumber,
+  }
+
+  res.json(userInput)
+})
+
 export default signup
