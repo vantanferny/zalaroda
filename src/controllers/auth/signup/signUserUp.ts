9628d6695fddecbdf7d164887c02c17492a57435
Anferny Vanta <vantanferny@github.com>
2020-03-01 16:09:06 +0800

    finished signup flow

--- src/controllers/auth/signup/signUserUp.ts
+++ src/controllers/auth/signup/signUserUp.ts
@@ -1,13 +1,29 @@
-import { UserSignupInput, ParsedUserSignupInput, ValidationResult, WriteQueryResult } from '../../../types'
+import {
+  UserSignupInput,
+  ParsedUserSignupInput,
+  ValidationResult,
+  WriteQueryResult,
+  Flash,
+  LoginCredentials,
+  AuthenticationResult
+} from '../../../types'
 import { validateSignupInput, parseSignUpInput } from '../../util'
 import { User } from '../../../models'
+import authenticateLoginCredentials from '../login/authenticateLoginCredentials'
 
 const signUserUp =  async (req, res) => {
   const signupInput: UserSignupInput = req.body
   const validationResult: ValidationResult = validateSignupInput(signupInput)
 
   if (!validationResult.valid) {
-    res.render('auth/signup', { params: signupInput, errors: validationResult.errors })
+    const flash: Flash = {
+      type: 'error',
+      message: validationResult.errors.join('\n')
+    }
+
+    req.session.flash = flash
+    req.session.params = signupInput
+    res.redirect('/signup')
 
     return
   }
@@ -15,7 +31,38 @@ const signUserUp =  async (req, res) => {
   const parsedSignupInput: ParsedUserSignupInput = await parseSignUpInput(signupInput)
   const userCreationResult: WriteQueryResult = await User.create(parsedSignupInput)
 
-  res.json({ userCreationResult: userCreationResult }) // log in and redirect to home
+  if (userCreationResult.success) {
+    const loginCredentials: LoginCredentials = {
+      email: signupInput.email,
+      password: signupInput.password,
+    }
+    const authenticationResult: AuthenticationResult = await authenticateLoginCredentials(loginCredentials)
+    const flash: Flash = {
+      type: 'success',
+      message: "Signup Successful."
+    }
+
+    if (authenticationResult.success) {
+      req.session.user = authenticationResult.sessionUser
+    } else {
+      flash.type = "error"
+      flash.message = authenticationResult.error
+    }
+
+    req.session.flash = flash
+
+    res.redirect('/')
+  } else {
+    const flash: Flash = {
+      type: 'error',
+      message: userCreationResult.error
+    }
+
+    req.session.flash = flash
+    req.session.params = signupInput
+
+    res.redirect('/signup')
+  }
 }
 
 export default signUserUp
